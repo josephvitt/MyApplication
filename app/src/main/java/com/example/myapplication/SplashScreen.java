06b2d1c522f38879b1804b8c8efcc21475670ac4
gorden <419514268@qq.com>
2022-11-10 22:54:43 +0800

    MyApplication

---
+++ app/src/main/java/com/example/myapplication/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    TextView appname;
+    LottieAnimationView lottie;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        appname = findViewById(R.id.app_name);
+        lottie = findViewById(R.id.lottie);
+
+        appname.animate().translationX(-1400).setDuration(2700).setStartDelay(0);
+        lottie.animate().translationX(2000).setDuration(2000).setStartDelay(2900);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        },5000);
+    }
+}
